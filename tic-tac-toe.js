78ccc6468f36b530cf090b3c37af5ab869696bb4
MayankVerma007 <72990085+MayankVerma007@users.noreply.github.com>
2022-07-27 19:53:29 +0530

    Update tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,2 +1,15 @@
-var name = prompt("Please enter your name: ");
-console.log(name);
+var arr = [["1:","2:","3:"],["4:","5:","6:"],["7:","8:","9:"]];
+var randarr = [1,2,3,4,5,6,7,8,9];
+
+for ( var i in arr){
+    console.log(arr[i]);
+}
+
+var input1 = prompt("Please specify the position where you want to enter your cross:");
+
+console.log(arr[Math.floor((parseInt(input1)-1)/3)][((parseInt(input1)-1)%3)]);
+arr[Math.floor((parseInt(input1)-1)/3)][((parseInt(input1)-1)%3)] = arr[Math.floor((parseInt(input1)-1)/3)][((parseInt(input1)-1)%3)] + 'X';
+
+for ( var i in arr){
+    console.log(arr[i]);
+}
