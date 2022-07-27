7d9f483801c9992685c4cae3ca42697204af26d4
MayankVerma007 <72990085+MayankVerma007@users.noreply.github.com>
2022-07-27 20:42:32 +0530

    Update attempt2.js

--- attempt2.js
+++ attempt2.js
@@ -1,19 +1,24 @@
+//Our basic TIC-TAC-TOE Grid
 var arr = [["1:","2:","3:"],["4:","5:","6:"],["7:","8:","9:"]];
+//This array is used to maintain track of positions filled in the grid 
 var randarr = [1,2,3,4,5,6,7,8,9];
 
-function printAll(arr){
+function printAll(arr){ 
+    //This functions prints our basic grid
     for ( var i in arr){
     console.log(arr[i]);
     }
 }
 function inputX(arr, randarr){
+    //This function takes user input and updates in main grid and also updates the 
+    //position used in position grid
     var input1 = prompt("Please specify the position where you want to enter your cross:");
 
-    console.log(arr[Math.floor((parseInt(input1)-1)/3)][((parseInt(input1)-1)%3)]);
     arr[Math.floor((parseInt(input1)-1)/3)][((parseInt(input1)-1)%3)] = arr[Math.floor((parseInt(input1)-1)/3)][((parseInt(input1)-1)%3)] + 'X';
-    console.log(randarr.splice(parseInt(input1)-1), 1);
+    randarr.splice((parseInt(input1)-1),1);
 }
 
 printAll(arr);
 inputX(arr, randarr);
+console.log(randarr);
 printAll(arr);
